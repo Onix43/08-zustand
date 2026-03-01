60c569f710494f134e567800fe631bb42af6fe49
Onix <piyvoda20033@gmail.com>
2026-03-01 20:40:12 +0200

    minor fixes

--- app/@modal/(.)notes/[id]/NotePreview.client.tsx
+++ app/@modal/(.)notes/[id]/NotePreview.client.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useQuery } from "@tanstack/react-query";
-import css from "./NoteDetails.module.css";
+import css from "./NotePreview.module.css";
 import { useParams } from "next/navigation";
 import { fetchNoteById } from "@/lib/api";
 import Modal from "@/components/Modal/Modal";
